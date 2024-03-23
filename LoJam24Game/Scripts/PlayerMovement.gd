284cede492c6df10997c51305314ec25015c4631
JamesJohnson2005 <jamejohn2005@gmail.com>
2024-03-22 21:11:42 -0400

    Scene Switching Works

--- LoJam24Game/Scripts/PlayerMovement.gd
+++ LoJam24Game/Scripts/PlayerMovement.gd
@@ -3,9 +3,11 @@ extends CharacterBody2D
 var speed = 400  # speed in pixels/sec
 var direction
 var mousePos = Vector2(0, 0)
+var playerPos = Vector2(0, 0)
 
 func _physics_process(delta):
 	mousePos = get_global_mouse_position()
 	look_at(mousePos)
 	direction = Input.get_vector("left", "right", "up", "down")
 	position += speed * delta * direction
+	playerPos = position
---
+++ LoJam24Game/Scripts/SceneSwitch.gd
@@ -0,0 +1,26 @@
+extends Node
+
+var inPresent = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func _input(event):
+	if event.is_action_pressed("SwapScene"):
+		SwitchScene();
+	
+func SwitchScene():
+	var playerInfo = get_node("/root/Scripts/PlayerMovement.gd")
+	
+	if inPresent == false:
+		get_tree().change_scene_to_file("res://Scenes/presentScene.tscn")
+	else:
+		get_tree().change_scene_to_file("res://Scenes/pastScene.tscn")
+		
+	inPresent = !inPresent
