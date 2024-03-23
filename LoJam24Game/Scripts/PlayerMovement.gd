3508a09f485b6a46827fb3d143a8a70166bf1e46
JamesJohnson2005 <jamejohn2005@gmail.com>
2024-03-22 20:50:24 -0400

    Player Movement and Looks at mouse

--- LoJam24Game/Scripts/PlayerMovement.gd
+++ LoJam24Game/Scripts/PlayerMovement.gd
@@ -2,7 +2,10 @@ extends CharacterBody2D
 
 var speed = 400  # speed in pixels/sec
 var direction
+var mousePos = Vector2(0, 0)
 
-func _process(delta):
+func _physics_process(delta):
+	mousePos = get_global_mouse_position()
+	look_at(mousePos)
 	direction = Input.get_vector("left", "right", "up", "down")
 	position += speed * delta * direction
