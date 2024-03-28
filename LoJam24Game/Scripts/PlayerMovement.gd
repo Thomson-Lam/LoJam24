2c7434df3a39732879fbd803f3113f9e9b8f7eb4
JamesJohnson2005 <jamejohn2005@gmail.com>
2024-03-28 12:46:36 -0400

    Press and hold to jump and charge

--- LoJam24Game/Scripts/PlayerMovement.gd
+++ LoJam24Game/Scripts/PlayerMovement.gd
@@ -3,26 +3,65 @@ extends RigidBody2D
 var speed = 400  # speed in pixels/sec
 var mousePos = Vector2(0, 0)
 var direction
-var rayCast
-var jumpHeight := 350
+var rayCastMiddle
+var rayCastLeft
+var rayCastRight
+var jumpHeight = 350
+var jumpTimer = 0
+var maxJumpTimer = 1
+var chargingJump = false
+var newJump = 0
 
 @onready var camera := get_viewport().get_camera_2d()
 
 func _ready():
 	camera = get_viewport().get_camera_2d()
-	rayCast = $CollisionShape2D/RayCast2D
+	rayCastMiddle = find_child("RayCastMiddle")
+	rayCastRight = find_child("RayCastRight")
+	rayCastLeft = find_child("RayCastLeft")
 	pass
 
 func _physics_process(delta):
 	direction = Input.get_vector("left", "right", "up", "down")
 	
-func _input(event):
+func _process(delta):
+	if chargingJump and jumpTimer < 1:
+		jumpTimer += delta
+		
+	if jumpTimer > 1:
+		jumpTimer = 1
 	
-	if rayCast.is_colliding():
+func _input(event):
+	newJump = jumpTimer * jumpHeight + 100
+	
+	if CheckGround():
 		if event.is_action_pressed("up"):
-			apply_central_impulse(Vector2(0, -jumpHeight * 1.5))
+			chargingJump = true
 		elif event.is_action_pressed("right"):
-			apply_central_impulse(Vector2(jumpHeight / 3, -jumpHeight))
+			chargingJump = true
 		elif event.is_action_pressed("left"):
-			apply_central_impulse(Vector2(-jumpHeight / 3, -jumpHeight))	
+			chargingJump = true
+			
+		if event.is_action_released("up"):
+			apply_central_impulse(Vector2(0, -newJump * 1.5))
+			chargingJump = false
+			jumpTimer = 0
+		elif event.is_action_released("right"):
+			apply_central_impulse(Vector2(newJump / 3, -newJump))
+			chargingJump = false
+			jumpTimer = 0
+		elif event.is_action_released("left"):
+			apply_central_impulse(Vector2(-newJump / 3, -newJump))	
+			chargingJump = false
+			jumpTimer = 0
+			
+			
+func CheckGround():
+	if rayCastMiddle.is_colliding():
+		return true
+	if rayCastRight.is_colliding():
+		return true
+	if rayCastLeft.is_colliding():
+		return true
+	return false
 
