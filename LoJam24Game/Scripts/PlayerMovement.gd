5f7502561ddc59becdc324bac269de45101473c5
JamesJohnson2005 <jamejohn2005@gmail.com>
2024-03-28 11:40:48 -0400

    Basic Fish movement

--- LoJam24Game/Scripts/PlayerMovement.gd
+++ LoJam24Game/Scripts/PlayerMovement.gd
@@ -1,49 +1,28 @@
-extends CharacterBody2D
+extends RigidBody2D
 
 var speed = 400  # speed in pixels/sec
-var direction
 var mousePos = Vector2(0, 0)
-var playerPos = Vector2(0, 0)
-var inPresent = true
-var offSet = 10000
-@export var switchOverlay : Node2D
-var canMove = true
+var direction
+var rayCast
+var jumpHeight := 350
 
 @onready var camera := get_viewport().get_camera_2d()
 
 func _ready():
 	camera = get_viewport().get_camera_2d()
-	switchOverlay.visible = false
+	rayCast = $CollisionShape2D/RayCast2D
 	pass
 
 func _physics_process(delta):
-	mousePos = get_global_mouse_position()
-	look_at(mousePos)
 	direction = Input.get_vector("left", "right", "up", "down")
-	if canMove:
-		position += speed * delta * direction
-	playerPos = position
 	
 func _input(event):
-	if event.is_action_pressed("SwapScene"):
-		SwapAreas()
 	
+	if rayCast.is_colliding():
+		if event.is_action_pressed("up"):
+			apply_central_impulse(Vector2(0, -jumpHeight * 1.5))
+		elif event.is_action_pressed("right"):
+			apply_central_impulse(Vector2(jumpHeight / 3, -jumpHeight))
+		elif event.is_action_pressed("left"):
+			apply_central_impulse(Vector2(-jumpHeight / 3, -jumpHeight))	
 
-func SwapAreas():
-	if camera == null:
-		camera = get_viewport().get_camera_2d()
-	if switchOverlay == null:
-		switchOverlay = get_node("CanvasOverlay/SwitchOverlay")
-	
-	if inPresent == true:
-		position += Vector2(offSet, 0)
-	else:
-		position -= Vector2(offSet, 0)
-	
-	switchOverlay.visible = true
-	canMove = false
-	await get_tree().create_timer(0.9).timeout
-	switchOverlay.visible = false
-	canMove = true
-
-	inPresent = !inPresent
