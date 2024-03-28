c311a009fe28a95bbf1ec0f718885962cbe60a46
Thomson-Lam <145819823+Thomson-Lam@users.noreply.github.com>
2024-03-28 12:12:35 -0400

    Working foundation
    Fixed files, commented out the UI because signal functions have not been made, will link later.

--- LoJam24Game/Scenes/UI/UI.gd
+++ LoJam24Game/Scenes/UI/UI.gd
@@ -4,25 +4,25 @@ class_name UI
 @onready var score_label = %Score
 @onready var bar = %Bar
 
-var score = 0: # Setter 
-	set(new_score):
-		score = new_score
-		_update_score_label()
+#var score = 0: # Setter 
+	#set(new_score):
+		#score = new_score
+		#_update_score_label()
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	_update_score_label()
+#func _ready():
+#	_update_score_label()
 
 
-func _update_score_label():
-	score_label.text = str(score)
+#func _update_score_label():
+	#score_label.text = str(score)
 
 
 # add link signal to update score 
-func _triggered_signal(collectable) -> void:
-	if collectable:
-		score += 1 # placeholder 
+#func _triggered_signal(collectable) -> void:
+	#if collectable:
+		#score += 1 # placeholder 
 
 
 
